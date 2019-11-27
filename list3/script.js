4aa8e1b6626527c7c3b3474b80d2bf7c84aacfb4
luk9400 <lukaszbratos@gmail.com>
2019-11-27 16:33:25 +0100

    refactor

--- list3/script.js
+++ list3/script.js
@@ -19,12 +19,11 @@ function init() {
         let sy = d * i;
         for (let j = 0; j < len; j++) {
             let sx = d * j;
-            puzzles.push({ sx: sx, sy: sy, red: false});
+            puzzles.push({ sx: sx, sy: sy, red: false });
             context.drawImage(image, sx, sy, d, d, sx, sy, d, d);
         }
     }
-
-    //puzzles = shuffleArray(puzzles)
+    puzzles = shuffleArray(puzzles)
     puzzles[0].red = true;
 }
 
@@ -32,7 +31,7 @@ function draw() {
     context.clearRect(0, 0, width, height);
     let xPos = 0;
     let yPos = 0;
-    puzzles.forEach( puzzle => {
+    puzzles.forEach(puzzle => {
         puzzle.xPos = xPos;
         puzzle.yPos = yPos;
         context.drawImage(image, puzzle.sx, puzzle.sy, d, d, xPos, yPos, d, d);
@@ -43,46 +42,36 @@ function draw() {
         }
     });
 
-    context.fillStyle = 'red';
+    context.fillStyle = '#ba2c59';
     context.fillRect(redPuzzle.xPos, redPuzzle.yPos, d, d);
 }
 
-function shuffleArray(o){
+function shuffleArray(o) {
     for (let j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 }
 
 function movePuzzle(mouseX, mouseY) {
-    puzzles.forEach( (puzzle, index) => {
-        if ((mouseX < puzzle.xPos) || (mouseX > (puzzle.xPos + d)) || (mouseY < puzzle.yPos) || (mouseY > (puzzle.yPos + d))) {
-            // no hit
-        } else if (!puzzle.red) {
-            // top
-            if ((index - len) >= 0 && puzzles[index - len].red) {
-                redPuzzle.xPos = puzzle.xPos;
-                redPuzzle.yPos = puzzle.yPos;
-                swap(puzzles, index, index - len);
-                draw();
-            } else
-            // bottom
-            if ((index + len) < (len * len) && puzzles[index + len].red) {
-                redPuzzle.xPos = puzzle.xPos;
-                redPuzzle.yPos = puzzle.yPos;
-                swap(puzzles, index, index + len);
-                draw();
-            } else
-            // right
-            if (((index + 1) % len) != 0 && puzzles[index + 1].red) {
-                redPuzzle.xPos = puzzle.xPos;
-                redPuzzle.yPos = puzzle.yPos;
-                swap(puzzles, index, index + 1);
-                draw();
-            } else
-            // left
-            if (((index - 1) % len) != (len - 1) && index != 0 && puzzles[index - 1].red) {
-                redPuzzle.xPos = puzzle.xPos;
-                redPuzzle.yPos = puzzle.yPos;
-                swap(puzzles, index, index - 1);
+    puzzles.forEach((puzzle, index) => {
+        if (mouseX > puzzle.xPos && mouseX < (puzzle.xPos + d) && mouseY > puzzle.yPos && mouseY < (puzzle.yPos + d)) {
+            if (!puzzle.red) {
+                if ((index - len) >= 0 && puzzles[index - len].red) { // top
+                    redPuzzle.xPos = puzzle.xPos;
+                    redPuzzle.yPos = puzzle.yPos;
+                    swap(puzzles, index, index - len);
+                } else if ((index + len) < (len * len) && puzzles[index + len].red) { // bottom
+                    redPuzzle.xPos = puzzle.xPos;
+                    redPuzzle.yPos = puzzle.yPos;
+                    swap(puzzles, index, index + len);
+                } else if (((index + 1) % len) != 0 && puzzles[index + 1].red) { // right
+                    redPuzzle.xPos = puzzle.xPos;
+                    redPuzzle.yPos = puzzle.yPos;
+                    swap(puzzles, index, index + 1);
+                } else if (((index - 1) % len) != (len - 1) && index != 0 && puzzles[index - 1].red) { // left
+                    redPuzzle.xPos = puzzle.xPos;
+                    redPuzzle.yPos = puzzle.yPos;
+                    swap(puzzles, index, index - 1);
+                }
                 draw();
             }
         }
@@ -92,17 +81,17 @@ function movePuzzle(mouseX, mouseY) {
 function swap(arr, a, b) {
     let tmp = arr[b];
     arr[b] = arr[a];
-    arr[a] = tmp; 
+    arr[a] = tmp;
 }
 
 init();
 draw();
 canvas.addEventListener('click', e => {
-    if(e.layerX || e.layerX == 0){
+    if (e.layerX || e.layerX == 0) {
         mouseX = e.layerX - canvas.offsetLeft;
         mouseY = e.layerY - canvas.offsetTop;
     }
-    else if(e.offsetX || e.offsetX == 0){
+    else if (e.offsetX || e.offsetX == 0) {
         mouseX = e.offsetX - canvas.offsetLeft;
         mouseY = e.offsetY - canvas.offsetTop;
     }
