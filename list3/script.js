b1fd9be691f53469ae40d118d7fbe59f2bc9a73a
luk9400 <luk9400@gmail.com>
2019-12-02 14:38:07 +0100

    galerka i niesprawne mieszanie

--- list3/script.js
+++ list3/script.js
@@ -2,30 +2,32 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
 let image = new Image();
-image.src = './images/pic1.png';
+image.src = './images/pic1.jpg';
 
-const width = 400;
-const height = 400;
-const len = 4;
-const d = width / len;
+const width = image.naturalWidth;
+const height = image.naturalHeight;
+const canvasWidth = canvas.width;
+let len = 4;
+let sWidth = width / len;
+let dWidth = canvas.width / len;
 let puzzles = [];
-let redPuzzle = { xPos: 0, yPos: 0 };
+let redPuzzle = { xPos: 0, yPos: 0, index: 0 };
 
 function init() {
+    let ratio = width / canvasWidth;
     for (let i = 0; i < len; i++) {
-        let sy = d * i;
+        let sy = dWidth * i;
         for (let j = 0; j < len; j++) {
-            let sx = d * j;
-            puzzles.push({ sx: sx, sy: sy, red: false, hovered: false });
-            context.drawImage(image, sx, sy, d, d, sx, sy, d, d);
+            let sx = dWidth * j;
+            puzzles.push({ sx: sx * ratio, sy: sy * ratio, red: false, hovered: false });
+            context.drawImage(image, sx * ratio, sy * ratio, sWidth, sWidth, sx, sy, dWidth, dWidth);
         }
     }
-    puzzles = shuffleArray(puzzles)
     puzzles[0].red = true;
 }
 
 function draw() {
-    context.clearRect(0, 0, width, height);
+    context.clearRect(0, 0, canvasWidth, canvasWidth);
     let xPos = 0;
     let yPos = 0;
     puzzles.forEach(puzzle => {
@@ -35,46 +37,119 @@ function draw() {
             context.save();
             context.globalAlpha = 0.5;
         }
-        context.drawImage(image, puzzle.sx, puzzle.sy, d, d, xPos, yPos, d, d);
+        context.drawImage(image, puzzle.sx, puzzle.sy, sWidth, sWidth, puzzle.xPos, puzzle.yPos, dWidth, dWidth);
         if (puzzle.hovered) {
             context.restore();
             puzzle.hovered = false;
         }
-        xPos += d;
-        if (xPos >= width) {
+        xPos += dWidth;
+        if (xPos >= canvasWidth) {
             xPos = 0;
-            yPos += d;
+            yPos += dWidth;
         }
     });
 
     context.fillStyle = '#ba2c59';
-    context.fillRect(redPuzzle.xPos, redPuzzle.yPos, d, d);
+    context.fillRect(redPuzzle.xPos, redPuzzle.yPos, dWidth, dWidth);
 }
 
-function shuffleArray(o) {
-    for (let j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
+function shuffle() {
+    for (let i = 0; i < Math.pow(3, len); i++) {
+        let dir = Math.floor(Math.random() * Math.floor(4));
+        let idx = redPuzzle.index
+        console.log('dlugosc: ' + puzzles.length)
+        console.log('idx: ' + idx);
+        console.log('len: ' + len);
+
+        switch (dir) {
+            case 0: { //top
+                console.log(idx - len >= 0)
+                if (idx - len >= 0) {
+                    redPuzzle.xPos = puzzles[idx - len].xPos;
+                    redPuzzle.yPos = puzzles[idx - len].yPos;
+                    redPuzzle.index -= len;
+                    swap(puzzles, idx, idx - len);
+                } else {
+                    redPuzzle.xPos = puzzles[idx + len].xPos;
+                    redPuzzle.yPos = puzzles[idx + len].yPos;
+                    redPuzzle.index += len;
+                    swap(puzzles, idx, idx + len);
+                }
+                break;
+            }
+            case 1: { //bottom
+                console.log(idx + len < len * len)
+                if (idx + len < len * len) {
+                    redPuzzle.xPos = puzzles[idx + len].xPos;
+                    redPuzzle.yPos = puzzles[idx + len].yPos;
+                    redPuzzle.index += len;
+                    swap(puzzles, idx, idx + len);
+                } else {
+                    redPuzzle.xPos = puzzles[idx - len].xPos;
+                    redPuzzle.yPos = puzzles[idx - len].yPos;
+                    redPuzzle.index -= len;
+                    swap(puzzles, idx, idx - len);
+                }
+                break;
+            }
+            case 2: { //right
+                if ((idx + 1) % len != 0 && idx + 1 < len) {
+                    redPuzzle.xPos = puzzles[idx + 1].xPos;
+                    redPuzzle.yPos = puzzles[idx + 1].yPos;
+                    redPuzzle.index += 1;
+                    swap(puzzles, idx, idx + 1);
+                } else {
+                    redPuzzle.xPos = puzzles[idx - 1].xPos;
+                    redPuzzle.yPos = puzzles[idx - 1].yPos;
+                    redPuzzle.index -= 1;
+                    swap(puzzles, idx, idx - 1);
+                }
+                break;
+            }
+            case 3: { //left
+                if (((idx - 1) % len) != (len - 1) && idx != 0) {
+                    redPuzzle.xPos = puzzles[idx - 1].xPos;
+                    redPuzzle.yPos = puzzles[idx - 1].yPos;
+                    redPuzzle.index -= 1;
+                    swap(puzzles, idx, idx - 1);
+                } else {
+                    redPuzzle.xPos = puzzles[idx + 1].xPos;
+                    redPuzzle.yPos = puzzles[idx + 1].yPos;
+                    redPuzzle.index += 1;
+                    swap(puzzles, idx, idx + 1);
+                }
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
 }
 
 function movePuzzle(mouseX, mouseY) {
     puzzles.forEach((puzzle, index) => {
-        if (mouseX > puzzle.xPos && mouseX < (puzzle.xPos + d) && mouseY > puzzle.yPos && mouseY < (puzzle.yPos + d)) {
+        if (mouseX > puzzle.xPos && mouseX < (puzzle.xPos + dWidth) && mouseY > puzzle.yPos && mouseY < (puzzle.yPos + dWidth)) {
             if (!puzzle.red) {
                 if ((index - len) >= 0 && puzzles[index - len].red) { // top
                     redPuzzle.xPos = puzzle.xPos;
                     redPuzzle.yPos = puzzle.yPos;
+                    redPuzzle.index = index;
                     swap(puzzles, index, index - len);
                 } else if ((index + len) < (len * len) && puzzles[index + len].red) { // bottom
                     redPuzzle.xPos = puzzle.xPos;
                     redPuzzle.yPos = puzzle.yPos;
+                    redPuzzle.index = index;
                     swap(puzzles, index, index + len);
                 } else if (((index + 1) % len) != 0 && puzzles[index + 1].red) { // right
                     redPuzzle.xPos = puzzle.xPos;
                     redPuzzle.yPos = puzzle.yPos;
+                    redPuzzle.index = index;
                     swap(puzzles, index, index + 1);
                 } else if (((index - 1) % len) != (len - 1) && index != 0 && puzzles[index - 1].red) { // left
                     redPuzzle.xPos = puzzle.xPos;
                     redPuzzle.yPos = puzzle.yPos;
+                    redPuzzle.index = index;
                     swap(puzzles, index, index - 1);
                 }
                 draw();
@@ -85,7 +160,7 @@ function movePuzzle(mouseX, mouseY) {
 
 function hoverPuzzle(mouseX, mouseY) {
     puzzles.forEach((puzzle, index) => {
-        if (mouseX > puzzle.xPos && mouseX < (puzzle.xPos + d) && mouseY > puzzle.yPos && mouseY < (puzzle.yPos + d)) {
+        if (mouseX > puzzle.xPos && mouseX < (puzzle.xPos + dWidth) && mouseY > puzzle.yPos && mouseY < (puzzle.yPos + dWidth)) {
             if (!puzzle.red) {
                 if ((index - len) >= 0 && puzzles[index - len].red) { // top
                     puzzle.hovered = true;
@@ -107,8 +182,11 @@ function swap(arr, a, b) {
     arr[b] = arr[a];
     arr[a] = tmp;
 }
+
 image.addEventListener('load', e => {
     init();
+    draw();
+    shuffle();
     draw();
 });
 
@@ -116,8 +194,6 @@ canvas.addEventListener('click', e => {
     let rect = canvas.getBoundingClientRect();
     let mouseX = e.clientX - rect.left;
     let mouseY = e.clientY - rect.top;
-
-    console.log(mouseX + "   " + mouseY)
 
     movePuzzle(mouseX, mouseY);
 });
@@ -129,3 +205,27 @@ canvas.addEventListener('mousemove', e => {
 
     hoverPuzzle(mouseXh, mouseYh);
 });
+
+document.querySelectorAll('div img').forEach(img => {
+    img.addEventListener('click', e => {
+        image.src = img.src;
+        document.getElementById('hint').src = img.src;
+        start();
+    })
+});
+
+document.getElementById('btn').addEventListener('click', e => {
+    len = document.getElementById('len').value;
+    start();
+});
+
+function start() {
+    puzzles = [];
+    redPuzzle = { xPos: 0, yPos: 0, index: 0 };
+    sWidth = width / len;
+    dWidth = canvas.width / len;
+    init();
+    draw();
+    shuffle();
+    draw();
+}
